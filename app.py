24b68a20045c62c9673e60b293d9a601d2564da3
hiderinrin <97324292+hiderinrin@users.noreply.github.com>
2023-04-24 22:48:29 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ if st.session_state["messages"]:
 custom_css = '''
 <style>
     .container {
-        background-color: #1a1a1a;
+        background-color: #7a7a7a;
         border-radius: 10px;
         padding: 10px;
         display: flex;
